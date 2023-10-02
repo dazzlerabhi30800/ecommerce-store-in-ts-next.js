460d2c50ac69145614e95add36d85e5bae9281ac
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-10-02 13:36:51 +0530

    added add to cart & and cart state change functionality

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,17 +1,57 @@
+"use client";
+import { useProductStore } from "@/store/ProductStore";
 import { INTEGER_FORMATTER } from "@/utils/utils";
 import Image from "next/image";
 
 export default function Product({ product }: { product: product }) {
+    const [products, setProducts, setCart] = useProductStore((state) => [state.products, state.setProducts, state.setCart]);
+
+
+    // function to convert the discount into price
+    const handleDiscount = (price: number, discount: number) => {
+        const discountedPrice = Math.floor( (discount * price) / 100 );
+        return price - discountedPrice;
+    }
+
+
+    // function to handle add to cart functionality
+    const handleAddToCart = (id: number) => {
+        setProducts(products.map((item) => {
+            if(item.id === id) {
+                return {...item, quantity: item.quantity + 1}
+            }
+            return item;
+        }))
+        setCart();
+    }
+
+
+    // function to handle remove cart functionality
+    const handleMinusCart = (id: number) => {
+        setProducts(products.map((item) => {
+            if(item.id === id) {
+                return {...item, quantity: item.quantity - 1 <= 0 ? 0 : item.quantity - 1}
+            }
+            return item;
+        }))
+        setCart();
+    }
+
+
+
     return (
-        <div className="product shadow-lg flex flex-col text-center justify-center gap-4 p-4 bg-white rounded-md min-w-[300px] w-full max-w-[350px] h-[420px]">
+        <div className="product shadow-lg flex flex-col text-center justify-between gap-4 p-4 bg-white rounded-md min-w-[300px] w-full max-w-[350px] h-[420px]">
             <Image className="productImg" quality={100} style={{ width: "auto", height: "auto", margin: "5px auto" }} src={product.image} width={125} height={35} alt={product.name} />
             <h1 className="font-bold">{product.name}</h1>
             <div className="flex bg-gray-400 rounded-lg items-center text-white w-fit mx-auto font-medium">
-                <button className="py-2 px-4 border-r-2 border-white">-</button>
+                <button onClick={() => handleMinusCart(product.id)} className="py-2 px-4 border-r-2 border-white">-</button>
                 <span className="py-2 px-4 border-r-2 border-white">{product.quantity}</span>
-                <button className="py-2 px-4">+</button>
+                <button onClick={() => handleAddToCart(product.id)} className="py-2 px-4">+</button>
             </div>
-            <span className="font-medium text-gray-800">₹{INTEGER_FORMATTER.format(product.price)}</span>
+            <div className="priceContainer font-medium flex items-center gap-4 justify-center">
+            <span className="line-through  text-gray-500">₹{INTEGER_FORMATTER.format(product.price)}</span>
+            <span>₹{INTEGER_FORMATTER.format(handleDiscount(product.price, product.discount))}</span>
+            </div>
         </div>
     )
 
--- store/ProductStore.tsx
+++ store/ProductStore.tsx
@@ -4,9 +4,19 @@ import data from '@/data/products.json';
 
 interface ProductState {
     products: product[],
+    cart: product[]
+    setProducts: (payload: product[]) => void;
+    setCart: () => void;
 }
 
 
-export const useProductStore = create<ProductState>((set) => ({
+export const useProductStore = create<ProductState>((set, get) => ({
     products: data.products,
+    cart: [],
+    setProducts: (payload) => set((state) => ({products: payload})),
+    setCart: () => {
+        const products = get().products;
+        const cart = products.filter((product) => product.quantity > 0);
+        set({cart});
+    }, 
 }))
