c2637ff31904d41b518f5d80caed807cb71465ee
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-10-02 12:43:52 +0530

    new commit in ubuntu

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useProductStore } from "@/store/ProductStore"
 export default function Home() {
   const [products] = useProductStore((state) => [state.products])
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4 place-items-center gap-4 p-2">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4 place-items-center gap-4 py-5 px-2">
       {products.map((product, index) => {
         return <Product key={index} product={product} />
       })}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <nav className="py-2 px-2 md:px-4 flex justify-between items-center">
+        <nav className="py-2 px-2 md:px-6 flex justify-between items-center">
             <img src="./logo.svg" alt="shopsy" className="w-auto h-[45px] " />
             <div className="links--container text-sm md:text-lg flex gap-4 items-center">
                 <a className="font-medium hover:underline hover:text-gray-600" href="#">Home</a>
